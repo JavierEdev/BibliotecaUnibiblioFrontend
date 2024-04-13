b009ff91eb52237fabf74dfc9c6f332cf1b74747
Javier Estrada <estradajuarezjavier01@gmail.com>
2024-04-13 11:49:24 -0600

    Commit #5: Agregado clase de conexión a la BD y agregado JSON del directorio con la cadena de conexión

---
+++ BibliotecaUnibiblioMVC/Datos/Conexion.cs
@@ -0,0 +1,25 @@
+﻿namespace BibliotecaUnibiblioMVC.Datos
+{
+    public class Conexion
+    {
+        private string cadenaSQL = string.Empty;
+        public Conexion()
+        {
+
+            var currentDirectory = Directory.GetCurrentDirectory();
+            Directory.SetCurrentDirectory(currentDirectory);
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(currentDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            cadenaSQL = builder.GetSection("ConnectionStrings:CadenaSQL").Value;
+
+        }
+        public string getCadenaSQL()
+        {
+            return cadenaSQL;
+        }
+    }
+}
