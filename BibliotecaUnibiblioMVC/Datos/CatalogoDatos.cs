02a366713d0d293000839ca62178a4ce1bb91b84
JostinLinares03 <jostinlinares2003@gmail.com>
2024-04-23 20:33:54 -0600

    Implementacion Catalogo

---
+++ BibliotecaUnibiblioMVC/Datos/CatalogoDatos.cs
@@ -0,0 +1,49 @@
+﻿using BibliotecaUnibiblioMVC.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace BibliotecaUnibiblioMVC.Datos
+{
+    public class CatalogoDatos
+    {
+
+        public List<CatalogoModel> ListarCatalogo()
+        {
+
+            //leer Tabla Libros(Select)
+
+            var oLista = new List<CatalogoModel>();
+
+            var cn = new Conexion();
+
+            using (var Conexion = new SqlConnection(cn.getCadenaSQL()))
+            {
+                Conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_SelectLibros", Conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        oLista.Add(new CatalogoModel()
+                        {
+                            idLibro = Convert.ToInt32(dr["ID_Departamento"]),
+                            Nombre = dr["Nombre"].ToString(),
+                            Autor = dr["Autor"].ToString(),
+                            anioPublicacion = dr["Año Publicacion"].ToString(),
+                            grupoLibro = dr["Grupo Libro"].ToString(),
+                            enStock = dr["En Stock"].ToString(),
+                            estado = dr["Estado"].ToString(),
+                            tipoIdentificador = dr["Tipo de identificador"].ToString(),
+                            idArea = dr["ID Area"].ToString()
+                        });
+                    }
+                }
+            }
+            return oLista;
+        }
+
+
+    }
+}
---
+++ BibliotecaUnibiblioMVC/Models/CatalogoModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibliotecaUnibiblioMVC.Models
+{
+    public class CatalogoModel
+    {
+        public int idLibro { get; set; }
+        public string Nombre { get; set; }
+        public string Autor { get; set; }
+        public string anioPublicacion { get; set; }
+        public string grupoLibro { get; set; }
+        public string enStock { get; set; }
+        public string estado { get; set; }
+        public string tipoIdentificador { get; set; }
+        public string idArea { get; set; }
+
+    }
+}
