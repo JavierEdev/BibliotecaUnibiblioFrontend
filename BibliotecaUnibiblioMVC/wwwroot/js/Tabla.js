86a970c404aa3d475909a946065236236e3df0f8
Byron Coc <cocpalomobyronalejandro@gmail.com>
2024-05-01 22:57:35 -0600

    Creacion pagina ver usuarios lectores y Actulizacion navbar
    Archivos creados:
    html
    css
    js
    para usuarios lectores
    
    modificaciones:
    Se actuaalizo la navbar en prestamos administrador a la version mas reciente

---
+++ BibliotecaUnibiblioMVC/wwwroot/js/Tabla.js
@@ -0,0 +1,10 @@
+﻿new DataTable('#example', {
+    language: {
+        info: 'Mostrando pagina _PAGE_ de _PAGES_',
+        infoEmpty: 'No hay informacion disponible',
+        infoFiltered: '(No hay informacion disponible)',
+        lengthMenu: 'Ver _MENU_ registros por pagina',
+        zeroRecords: 'No se encontraron registros',
+        search: 'Buscar:',
+    }
+});
